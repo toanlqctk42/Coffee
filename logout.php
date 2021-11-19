2d94253cafa185682342989d8227235e69ad6f49
toanlqctk42 <toanlqctk42@gmail.com>
2021-11-19 14:33:42 +0700

    update coffee

--- logout.php
+++ logout.php
@@ -2,10 +2,5 @@
 session_start();
 
 unset($_SESSION['uid']);
-// if(isset($_SESSION['uid']) && $_SESSION(['uid'])!= NULL)
-// {
-//     unset($_SESSION['uid']);
-//     echo "Đăng xuất thành công";
-    header("Location:login.php");
-// }
+header("Location:login.php");
 ?>
